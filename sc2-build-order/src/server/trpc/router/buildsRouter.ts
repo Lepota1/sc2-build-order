5c33a0f536fa21eedd668569bb72f6c815271a38
Yauheni Lapets <lapezbyby@gmail.com>
2023-01-10 12:57:16 +0100

    added build sub,it and view build functianality

---
+++ sc2-build-order/src/server/trpc/router/buildsRouter.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+import { router, publicProcedure } from "../trpc";
+
+export const buildsRouter = router({
+  createBuild: publicProcedure
+    .input(
+      z.object({
+        matchUp: z.string(),
+        build: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const build = await ctx.prisma.buildOrder.create({
+        data: {
+          matchUp: input.matchUp,
+          build: input.build,
+        },
+      });
+
+      return build;
+    }),
+  getBuilds: publicProcedure.query(async ({ ctx }) => {
+    const builds = await ctx.prisma.buildOrder.findMany();
+    return builds;
+  }),
+});
